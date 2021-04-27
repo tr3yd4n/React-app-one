48e85b7d033d4f8304fe22dcb6b66724a39601c5
tr3yd4n <80318628+tr3yd4n@users.noreply.github.com>
2021-04-27 23:32:37 +0100

    Add files via upload

---
+++ src/components/MyNavBar.jsx
@@ -0,0 +1,17 @@
+import { Navbar, Nav } from 'react-bootstrap'
+
+const MyNavBar = () => (
+    <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
+    </Navbar>
+
+)
+
+export default MyNavBar
